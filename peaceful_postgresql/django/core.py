59f1710e5b9464146f95193c26f69fad76a6e201
Efe Öge <efeoge@gmail.com>
2025-03-20 22:50:16 +0100

    Django management command is implemented.

--- peaceful_postgresql/django/core.py
+++ peaceful_postgresql/django/core.py
@@ -1,16 +1,20 @@
 from django.db.migrations.recorder import MigrationRecorder
 from django.apps import apps
 
-# Get all applied migrations
-applied_migrations = set(migration.name for migration in MigrationRecorder.objects.all())
+def get_unapplied_migrations():
+    # Get all applied migrations
+    applied_migrations = set(migration.name for migration in MigrationRecorder.objects.all())
 
-# Get all existing migrations
-all_migrations = set(
-    migration for app in apps.get_app_configs() for migration in app.migrations
-)
+    # Get all existing migrations
+    all_migrations = set(
+        migration for app in apps.get_app_configs() for migration in app.migrations
+    )
 
-# Find unapplied migrations
-unapplied_migrations = all_migrations - applied_migrations
-print(unapplied_migrations)
+    # Find unapplied migrations
+    unapplied_migrations = all_migrations - applied_migrations
+    return unapplied_migrations
 
-sql_statements = sqlmigrate(app_label, migration_name)
+
+def get_sql_statements_of_migration(app_label, migration_name):
+    sql_statements = sqlmigrate(app_label, migration_name)
+    return sql_statements
---
+++ peaceful_postgresql/django/management/commands/detect_migration_locks.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from peaceful_postgresql.django.core import get_sql_statements_of_migration, get_unapplied_migrations
+from peaceful_postgresql.query import detect_locks
+
+class Command(BaseCommand):
+    help = 'Detects potential locks that could occur during a migration'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--app_label', type=str, help='The app label of the migration', nargs='?', default=None
+        )
+        parser.add_argument(
+            '--migration_name', type=str, help='The name of the migration', nargs='?', default=None
+        )
+
+    def handle(self, *args, **options):
+        app_label = options.get('app_label')
+        migration_name = options.get('migration_name')
+
+        if app_label and migration_name:
+            migrations = [(app_label, migration_name)]
+        else:
+            migrations = get_unapplied_migrations()
+
+        for migration in migrations:
+            # Get SQL statements for the migration
+            sql_statements = get_sql_statements_of_migration(app_label, migration_name)
+
+            # Detect locks for each SQL statement
+            for sql in sql_statements.split(';'):
+                sql = sql.strip()
+
+                if not sql:
+                    # Skip empty statements
+                    continue
+
+                locks = detect_locks(sql)
+                if locks:
+                    self.stdout.write(f"\nAnalyzing SQL: {sql}")
+                    self.stdout.write(self.style.WARNING("Potential locks detected:"))
+                    for lock in locks:
+                        self.stdout.write(f"- {lock}")
+                else:
+                    self.stdout.write(self.style.SUCCESS(f"No locks detected for: {sql}"))
+
+    
